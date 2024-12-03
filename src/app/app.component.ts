44140df6b97b1d7d4af5fbd579b770038f353914
lucasgfaj <lucasgfajardo09@gmail.com>
2024-12-03 16:49:34 -0300

    Finish Firs App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from './home/home.component';
-import { RouterLink, RouterModule } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent, RouterModule, RouterLink]
+  standalone: true,
+  imports: [
+    RouterLink,
+    RouterOutlet
+],
   template: `
-   <main>
+    <main>
       <a [routerLink]="['/']">
         <header class="brand-name">
-          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
         </header>
       </a>
       <section class="content">
